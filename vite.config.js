879ad3a29b98bd6bd78546ce5195513373eb0333
Mario-Estoque@01 <Mario@GRAMOTERRA.local>
2025-06-10 14:25:36 -0300

    ok

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,13 @@
-import { defineConfig } from 'vite';
+// vite.config.js
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  // Your Vite configuration
-});
+  plugins: [
+    react(),
+    // NENHUM PLUGIN RELACIONADO A TAILWIND/POSTCSS AQUI
+  ],
+  build: {
+    chunkSizeWarningLimit: 1000,
+  },
+})
