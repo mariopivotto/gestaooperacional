23d7f14486c43cb4b36dde720d57a011a0357ea1
Mario-Estoque@01 <Mario@GRAMOTERRA.local>
2025-06-10 11:44:27 -0300

    Fix: Adiciona configuração do PostCSS para teste

--- postcss.config.js
+++ postcss.config.js
@@ -1,8 +1,6 @@
-// Dentro do arquivo postcss.config.js
-
-export default {
-  plugins: {
-    '@tailwindcss/postcss': {},
-    autoprefixer: {},
-  },
-}
+module.exports = {
+  plugins: [
+    require('@tailwindcss/postcss'),
+    require('autoprefixer'),
+  ],
+};
